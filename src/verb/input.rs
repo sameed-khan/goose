385e67187a58c1d09a498ed44a167d2debe0cd49
Sameed Khan <sameed.khan@case.edu>
2024-09-06 04:32:07 -0400

    feat[verb]: Add input
    Also added first test for input function using AbsoluteCoordinates.

--- src/verb/input.rs
+++ src/verb/input.rs
@@ -1,6 +1,7 @@
 use crate::errors::UIActionTimeOutError;
 use crate::nav::location::convert_bitmap_to_mat;
 use crate::nav::location::LocationStrategy;
+use crate::nav::location::PointAsRectAnchor;
 use crate::verb::action::{CheckUIState, GuiAction, GuiVerb};
 use autopilot::bitmap::Bitmap;
 use autopilot::{
@@ -50,7 +51,9 @@ impl<L: LocationStrategy> GuiAction for Input<L> {
         let tmp = self.get_screenshot()?;
         let location: Point = (self.target.get_location(&tmp)?).into();
 
+        dbg!(mouse::location());
         mouse::move_to(location)?;
+        dbg!(mouse::location());
         mouse::click(Button::Left, None);
         let screenshot = self.get_screenshot()?;
         key::type_string(&self.input_string, &[], 60.0, 0.0);
@@ -69,13 +72,11 @@ impl<L: LocationStrategy> GuiVerb for Input<L> {
             Some(rect) => rect,
             None => {
                 let tmp = self.get_screenshot().expect("Unable to screenshot");
-                let template_location: Point = (self
+                let template_location = self
                     .target
                     .get_location(&tmp)
-                    .expect("Unable to screenshot"))
-                .into();
-                let rect_size = Size::new(200.0, 200.0);
-                Rect::new(template_location, rect_size)
+                    .expect("Unable to get location");
+                template_location.generate_rect(150, 150, PointAsRectAnchor::Center)
             }
         }
     }
@@ -90,6 +91,9 @@ impl<L: LocationStrategy> GuiVerb for Input<L> {
         while timeout > 0 {
             thread::sleep(time::Duration::from_millis(wait_duration));
             after = self.get_screenshot()?;
+            dbg!(self.get_check_zone());
+            dbg!(before.bitmap_eq(&after, None));
+            dbg!(autopilot::screen::scale());
             if self.changed_ui_state(&before, &mut after, Some(self.get_check_zone()))? {
                 return Ok(());
             }
@@ -99,5 +103,49 @@ impl<L: LocationStrategy> GuiVerb for Input<L> {
         return Err(Box::new(UIActionTimeOutError {
             message: "UI action timed out".to_string(),
         }));
+    }
+}
+
+// NOTE: The tests below are flaky and WILL fail if not run using `cargo test -- --test-threads=1`
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::errors::UIActionTimeOutError;
+    use crate::nav::location::{AbsoluteLocation, ImageTemplate};
+    use std::process::Command;
+    use std::{panic, thread, time};
+
+    const DELAY_BETWEEN_TESTS: u64 = 1;
+
+    // Setup test fixture to open chrome.exe
+    fn setup() -> () {
+        Command::new("cmd")
+            .args(&["/C", "start", "", "/max", "notepad.exe"])
+            .spawn()
+            .expect("Failed to open notepad.exe");
+
+        thread::sleep(time::Duration::from_secs(1));
+    }
+
+    fn teardown() -> () {
+        Command::new("taskkill")
+            .args(&["/IM", "notepad.exe", "/F"])
+            .spawn()
+            .expect("Failed to close notepad.exe");
+    }
+
+    #[test]
+    fn input_by_coordinate() {
+        setup();
+        let input = Input::new(
+            AbsoluteLocation { x: 25, y: 100 },
+            "Hello, World!".to_string(),
+            None,
+            None,
+        );
+        let result = panic::catch_unwind(|| input.fire(None, None).expect("Failed to input text"));
+        thread::sleep(time::Duration::from_secs(DELAY_BETWEEN_TESTS));
+        teardown();
+        assert!(result.is_ok());
     }
 }
